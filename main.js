e56f6ada55a93480ca3d168786dbf3d03af5b810
tirumalad-del <tirumala.d@gmail.com>
2025-12-10 03:01:21 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadFile('index.html');
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
